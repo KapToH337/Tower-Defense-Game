5872f9b130dc411f83b24b9bb474c228c0c92b17
Artem Bazhenov <artem.bazhenov.325@mail.com>
2021-11-24 18:36:55 +0200

    add waves

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
+  public wave: number = 1;
+  public win: boolean = false;
+  public loose: boolean = false;
+
   public enemy: any = [];
   private id: number = 1;
+  private number: number = 1;
 
   constructor( ) {}
 
@@ -16,36 +21,57 @@ export class AppComponent implements OnInit {
     this.createEnemyOnWave();
   }
 
+  public goToNextWave() {
+    this.win = false;
+    this.wave += 1;
+    this.number = 1;
+    this.createEnemyOnWave();
+  }
+
+  public getDamage(id: number) {
+    this.enemy.map((obj: any) => {
+      if (obj.id === id) {
+        obj.health -= 25;
+      }
+    })
+
+    if (this.enemy.every((obj: any) => obj.health === 0) && this.id > 5 * this.wave) {
+      this.win = true;
+    }
+  }
+
   private createEnemyOnWave(): void {
     const enemy = {
       id: this.id,
       positionX: -100,
       positionY: 150,
-      health: 100
+      health: 100 * this.wave
     };
     this.enemy.push(enemy);
 
     setInterval(() => {
       if (this.enemy[enemy.id - 1].positionX < 400) {
-        this.enemy[enemy.id - 1].positionX += 100;
+        this.enemy[enemy.id - 1].positionX += 1;
       } else if (this.enemy[enemy.id - 1].positionX === 400 && this.enemy[enemy.id - 1].positionY !== 550) {
-        this.enemy[enemy.id - 1].positionY += 100;
+        this.enemy[enemy.id - 1].positionY += 1;
       } else if (this.enemy[enemy.id - 1].positionY === 550 && this.enemy[enemy.id - 1].positionX !== 900) {
-        this.enemy[enemy.id - 1].positionX += 100;
+        this.enemy[enemy.id - 1].positionX += 1;
       } else if (this.enemy[enemy.id - 1].positionX === 900 && this.enemy[enemy.id - 1].positionY !== 150) {
-        this.enemy[enemy.id - 1].positionY -= 100;
+        this.enemy[enemy.id - 1].positionY -= 1;
       } else if (this.enemy[enemy.id - 1].positionX !== 1300) {
-        this.enemy[enemy.id - 1].positionX += 100;
-      } else if (this.enemy[enemy.id - 1].positionX === 1300) {
+        this.enemy[enemy.id - 1].positionX += 1;
+      } else if (this.enemy[enemy.id - 1].positionX === 1300 && this.enemy[enemy.id - 1].health > 0) {
+        this.loose = true;
         return;
       }
-    }, 500);
+    }, 17);
 
     this.id += 1;
-    if (this.id <= 5) {
+    this.number += 1;
+    if (this.number <= 5 * this.wave) {
       setTimeout(() => {
         this.createEnemyOnWave();
-      }, 1500)
+      }, 3500)
     }
   }
 
